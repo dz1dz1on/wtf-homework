bfa94da971b025b514af94e665f69dd23c3c6045
krzysztofstepniak-silvair <stepniakkrzys@gmail.com>
2020-04-01 20:42:54 +0200

    feat()[]: add pulling repos from github

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,30 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+const list = document.querySelector(".projects-list--js");
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+fetch("https://api.github.com/users/dz1dz1on/repos?sort=updated")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    const repositories = resp;
+    for (const repo of repositories) {
+      const { description, html_url, name } = repo;
+      list.innerHTML += `
+      <li class="project">
+            <div class="project__container">
+              <img class="project__logo" src="../assets/img/github.svg" alt="">
+              <h3 class="project__title">${name}</h3>
+              <p class="project__description">
+              ${description ? description : "brak opisu"}
+              </p>
+            </div>
+            <div class="project__footer">
+              <a class="project__link" href="#">Demo</a>
+              <a class="project__link project__link--git" href="${html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
+            </div>
+      </li>
+      `;
+    }
+  })
+  .catch((error) => {
+    console.log(error);
   });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
